ad32b3211d0786bf082e54e48adbdada7b0199b1
Leivmn <Leivacrc@gmail.com>
2023-07-24 18:15:07 -0600

    corrections

--- condominium/src/main.jsx
+++ condominium/src/main.jsx
@@ -1,29 +1,20 @@
-<<<<<<< Updated upstream
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import "./index.css";
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import Form from './Form.jsx'
 import './index.css'
->>>>>>> Stashed changes
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<<<<<<< Updated upstream
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <Form/>
     </Provider>
   </React.StrictMode>
 );
-=======
-    {/*<App />*/}
-    <Form/>
-  </React.StrictMode>,
-)
->>>>>>> Stashed changes
