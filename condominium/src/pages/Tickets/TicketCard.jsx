3a8ea158cc64240ffd000349368aec03a2ed24a0
Raquel Juarez <rjmendez0518@gmail.com>
2023-08-24 10:19:13 -0600

    Update diseño cards

--- condominium/src/pages/Tickets/TicketCard.jsx
+++ condominium/src/pages/Tickets/TicketCard.jsx
@@ -1,13 +1,41 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import React from "react";
+import { Card, CardContent, Typography, Divider, Icon } from "@mui/material";
+import LavenderIcon from "@mui/icons-material/LocalFlorist"; // Import the Lavender icon
 
-const TicketCard = ({card}) => {
-  const { title, description, status }=card
+const TicketCard = ({ card }) => {
+  const { title, description, status } = card;
+
   return (
-    <Card>
+    <Card
+      sx={{
+        backgroundColor: "#FFFFFF",
+        boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.1)", // Increased shadow
+      }}
+    >
       <CardContent>
-        <Typography variant="h6">{title}</Typography>
-        <Typography variant="body2">{description}</Typography>
-        <Typography variant="subtitle1">Status: {status}</Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            textAlign: "center",
+            marginBottom: 1,
+            display: "flex",
+            alignItems: "center", // Center the icon and title
+            justifyContent: "center",
+          }}
+        >
+          <Icon
+            component={LavenderIcon}
+            sx={{ color: "#B57EDC", marginRight: 1 }} // Lilac purple color
+          />
+          {title}
+        </Typography>
+        <Divider sx={{ marginBottom: 2 }} /> {/* Increased space */}
+        <Typography variant="body2" sx={{ marginBottom: 1 }}>
+          {description}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ marginBottom: 1 }}>
+          Status: {status}
+        </Typography>
       </CardContent>
     </Card>
   );
