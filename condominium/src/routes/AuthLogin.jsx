21fb84fb617436e0ee035cc2f12771a1a155ac7e
Raquel Juarez <rjmendez0518@gmail.com>
2023-08-26 02:20:28 -0600

    Auth Login

--- condominium/src/routes/AuthLogin.jsx
+++ condominium/src/routes/AuthLogin.jsx
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux"
 import MainRoutes from "./MainRoutes";
+import Login from '../pages/Login'
 
 const AuthLogin = () => {
     const hasAcess = useSelector(state => state.actual.allowed);
-    return hasAcess ? MainRoutes : {path: '/*', element: <>Hola</>}
+    return hasAcess ? MainRoutes : {path: '/*', element: <Login/>}
 }
 
 export default AuthLogin;
